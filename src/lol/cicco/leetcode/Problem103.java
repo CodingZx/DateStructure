e69a599395b916007e2f62a0f1044db2d8c17b72
Cicco <codingzx@outlook.com>
2020-02-19 15:49:41 +0800

    add Problem103 中等 二叉树的锯齿形层次遍历

---
+++ src/lol/cicco/leetcode/Problem103.java
@@ -0,0 +1,36 @@
+package lol.cicco.leetcode;
+
+import lol.cicco.leetcode.struct.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Problem103 {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<>();
+        if (root == null) return levels;
+
+        processData(root, 0, levels);
+
+        return levels;
+    }
+
+    private void processData(TreeNode node, int level, List<List<Integer>> dataList) {
+        if(dataList.size() == level) {
+            dataList.add(new LinkedList<>());
+        }
+        if(level % 2 == 0) {
+            dataList.get(level).add(0, node.val);
+        } else {
+            dataList.get(level).add(node.val);
+        }
+
+        if(node.left != null) {
+            processData(node.left, level + 1, dataList);
+        }
+        if(node.right != null) {
+            processData(node.right, level + 1, dataList);
+        }
+    }
+}
