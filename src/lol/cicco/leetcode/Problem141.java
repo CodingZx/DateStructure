73a895196f067c407a9c010d36e5569f48e2167f
zhaoxu <zhaoxu@2019>
2020-02-24 09:51:41 +0800

    add Problem141 简单 环形链表

---
+++ src/lol/cicco/leetcode/Problem141.java
@@ -0,0 +1,19 @@
+package lol.cicco.leetcode;
+
+import lol.cicco.leetcode.struct.ListNode;
+
+public class Problem141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
