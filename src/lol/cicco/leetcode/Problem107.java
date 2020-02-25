4d18950b58d609811a09d56465c2479e936871ab
zhaoxu <zhaoxu@2019>
2020-02-25 09:44:02 +0800

    add Problem107 简单 二叉树的层次遍历 II

---
+++ src/lol/cicco/leetcode/Problem107.java
@@ -0,0 +1,33 @@
+package lol.cicco.leetcode;
+
+import lol.cicco.leetcode.struct.TreeNode;
+
+import java.util.*;
+
+public class Problem107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        while(!queue.isEmpty()) {
+            int len = queue.size();
+            List<Integer> vals = new ArrayList<>(len);
+            for(int i=0;i<len;i++){
+                TreeNode node = queue.poll();
+                if(node.left != null) {
+                    queue.add(node.left);
+                }
+                if(node.right != null) {
+                    queue.add(node.right);
+                }
+                vals.add(node.val);
+            }
+            res.add(vals);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
