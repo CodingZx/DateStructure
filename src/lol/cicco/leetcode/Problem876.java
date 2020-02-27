81358dff9d5aa0859ccabd37c3dad21c7ef2b484
zhaoxu <zhaoxu@2019>
2020-02-27 09:50:48 +0800

    add Problem876 简单 链表的中间结点

---
+++ src/lol/cicco/leetcode/Problem876.java
@@ -0,0 +1,31 @@
+package lol.cicco.leetcode;
+
+public class Problem876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+
+    public static class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
