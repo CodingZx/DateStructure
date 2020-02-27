b6b6b31216e995d2d4f4f42590a7ead68bf0134a
zhaoxu <zhaoxu@2019>
2020-02-27 10:09:35 +0800

    add Problem707 中等 设计链表

--- src/lol/cicco/Main.java
+++ src/lol/cicco/Main.java
@@ -8,17 +8,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Problem445 problem = new Problem445();
+        Problem707.MyLinkedList problem = new Problem707.MyLinkedList();
 
-        Problem445.ListNode node1 = new Problem445.ListNode(9);
-        Problem445.ListNode node2 = new Problem445.ListNode(1);
-        Problem445.ListNode node3 = new Problem445.ListNode(6);
-        Problem445.ListNode node4 = new Problem445.ListNode(0);
-        node1.next = node2;
-        node2.next = node3;
+        problem.addAtHead(1);
+        problem.addAtTail(3);
+        problem.addAtIndex(1, 2);
 
-        problem.addTwoNumbers(node1, node4);
-
+        System.out.println(problem.get(1));
+        problem.deleteAtIndex(1);
+        System.out.println(problem.get(1));
     }
 
 }
