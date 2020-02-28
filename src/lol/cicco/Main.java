22f121d1cd7bb68fc1c3fae7c7590dbe5f2f7ff6
Cicco <codingzx@outlook.com>
2020-02-28 22:54:52 +0800

    add Problem191 简单 位1的个数

--- src/lol/cicco/Main.java
+++ src/lol/cicco/Main.java
@@ -7,7 +7,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        Problem191 problem = new Problem191();
 
+        problem.hammingWeight(11);
     }
 
 }
---
+++ src/lol/cicco/leetcode/Problem191.java
@@ -0,0 +1,14 @@
+package lol.cicco.leetcode;
+
+public class Problem191 {
+    public int hammingWeight(int n) {
+        String binary = Integer.toBinaryString(n);
+        int count = 0;
+        for(int i=0;i<binary.length();i++) {
+            if(binary.charAt(i) == '1') {
+                count++;
+            }
+        }
+        return count;
+    }
+}
