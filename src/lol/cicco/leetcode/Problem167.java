b0b1224549b6774e58cc56bf8ba7d59fda6e32c8
Cicco <codingzx@outlook.com>
2020-02-28 22:48:16 +0800

    add Problem167 简单 两数之和 II - 输入有序数组

---
+++ src/lol/cicco/leetcode/Problem167.java
@@ -0,0 +1,22 @@
+package lol.cicco.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem167 {
+    public int[] twoSum(int[] nums, int target) {
+        // 存储 值-下标
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int rem = target - nums[i];
+            Integer val = hash.get(rem);
+            // 和已经存储的值比较 是否有相加等于target的值.
+            if (val != null) {
+                return new int[]{val+1, i+1};
+            }
+            // 无匹配项
+            hash.put(nums[i], i);
+        }
+        return null;
+    }
+}
